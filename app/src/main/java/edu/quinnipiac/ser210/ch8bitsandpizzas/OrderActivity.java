da4406492bfc6c42add7dcde02d15b5bf053d6ec
Alexandra Martin <amartin1@quinnipiac.edu>
2020-02-22 17:23:10 -0500

    Ch 8 Android Bits and Pizza

---
+++ app/src/main/java/edu/quinnipiac/ser210/ch8bitsandpizzas/OrderActivity.java
@@ -0,0 +1,28 @@
+package edu.quinnipiac.ser210.ch8bitsandpizzas;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+}
